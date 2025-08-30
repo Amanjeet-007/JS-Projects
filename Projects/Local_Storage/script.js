8e096cc5c2d21e98539a0c9b74f456217f4dc1b1
Amanjeet Kumar <kumaramanjeet951@gmail.com>
2025-08-30 21:38:40 +0530

    Todo LocalStorage Addeed

--- Projects/Local_Storage/script.js
+++ Projects/Local_Storage/script.js
@@ -1,70 +1,64 @@
 const btn = document.getElementById("add");
 const input = document.querySelector("input");
-const li = document.querySelector(".li")
+const list = document.querySelector(".li")
+const all_btn = document.getElementById("clear")
+const message = document.querySelector(".massage")
 
- //A array which has all index to access iteams from localStorage
-let all = []
-//if have any iteam then show 
+// localhost methods session storage (5mb)
+// > localStorage.setItem()
+// > localStorage.getItem()
+// > localStorage.removeItem()
+// > localStorage.updateIteam()
 
-btn.addEventListener("click",()=>{
-    //if input has value then exixute the code
-    if(input.value){
-    //getting value from input
-    let iteam = input.value;
-    //assin a index for that input
-    let id = 
-        console.log(id)
-    //Adding key(id) and value(iteam) in localStorage
-    localStorage.setItem(id, iteam);
-    //push same key in All array
-    all.push(id)
-            let newIteam;
-            newIteam += `
-            <li>
-                <div class="box" accesskey=${id}>
-                    <div class="iteam">${iteam}</div>
-                    <button class="delete">Delete</button>
-                </div>
-            </li>`
-            li.innerHTML += newIteam;
-            input.value = '';
-    }else{
-        console.log("Please Enter Some Text")
+// Show tasks
+function show() {
+    list.innerHTML = "";
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const value = localStorage.getItem(key);
+        if (!key) continue;
+        list.innerHTML += `
+        <li data-key="${key}">
+            <div class="text">${value}</div>
+            <button class="delete">Delete</button>
+        </li>`;
     }
-    
 
+    // attach delete listeners to each button
+    const deleteBtns = document.querySelectorAll(".delete");
+    deleteBtns.forEach(el => {
+        el.addEventListener("click", () => {
+            const item = el.parentElement;
+            const key = item.getAttribute("data-key");
+            localStorage.removeItem(key);
+            show(); // refresh after delete
+        });
+    });
+}
 
-    if(all.length >= 1){
-        all.forEach((el)=>{
-            let OneByOne = localStorage.getItem(el)
-           li.innerHTML +=  `<
-           li accesskey=${el}>
-            <div class="box">
-                <div class="iteam">${OneByOne}</div>
-                <button class="delete">Delete</button>
-            </div>
-        </li>`
-        })
+// Add task
+function add() {
+    if (!input.value) {
+        message.innerText = "* Please Add Text (content).";
+    } else {
+        message.innerText = "";
+        localStorage.setItem(Date.now(), input.value); // unique key
+        input.value = ""; // clear input
+        show();
     }
-    
+}
 
-    if(li.childNodes.length>5){
-        const Delete = document.querySelectorAll(".delete");
-       
-        Delete.forEach((el)=>{
-            el.addEventListener("click",()=>{
-                localStorage.removeItem(el.parentElement.parentNode.accessKey);
-               el.parentElement.parentNode.remove()
-            all.splice(el.parentElement.parentNode.accessKey-1,1)
+// Delete all tasks
+function all_delete() {
+    localStorage.clear();
+    list.innerHTML = "";
+}
 
-        })
-        })
-    }
-    
-})
+// Main function
+function main() {
+    show(); 
+    btn.addEventListener("click", add);
+    all_btn.addEventListener("click", all_delete);
+}
 
-
- 
-
-
-
+main();
